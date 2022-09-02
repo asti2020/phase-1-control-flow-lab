5584637ba6b3f43f36a0a356a6b10028f1797041
Astika Luintel <astild2020@gmail.com>
2022-09-01 20:15:21 -0400

    commit tree d42959ab5013f710c8e297715a859851691f54fc

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value1){
+
+  if(value1<= 400){
+    return `This one is on me!`;
+  }
+  else if(value1 <= 2000){
+    return `That will be twenty bucks.`;
+  }
+  else if(value1 < 2500){
+  return `I will gladly take your thirty bucks.`;
+  }
+  else {
+    return `No can do.`;
+  }
+ // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return `Ok, sounds good.`
+  }
+  else{
+    return `No go.`
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous") {
+    return `Thank you so much.`
+  }
+  else if(tip ==="not as generous"){
+    return `Thank you.`
+  }
+  else{
+    return `Bye.`
+  }
 }
